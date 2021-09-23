782a2055335da0952778df7843c67f18414a1489
Joe Alves <joseph.michael.alves@gmail.com>
2021-09-23 11:00:19 -0400

    demo code so far

--- program.js
+++ program.js
@@ -1,27 +1,59 @@
-const myWholeBusinessCard = document.querySelector("#business-card");
-const myNameElement = document.querySelector("h1");
+const businessCardElement = document.querySelector("#business-card");
+const phoneNumber = document.querySelector("#phone-number");
 
-console.dir(myNameElement);
+const makeMyCardPurple = () => {
+    businessCardElement.style.background = "purple";
+}
 
-myWholeBusinessCard.addEventListener("click", function () {
-    myWholeBusinessCard.classList.add("red");
-    const brandNewH2 = document.createElement("h2");
-    brandNewH2.innerText = "It's red now!";
-    myWholeBusinessCard.appendChild(brandNewH2);
-    setTimeout(() => {
-        myWholeBusinessCard.remove();
-        setTimeout(() => {
-            document.body.appendChild(myWholeBusinessCard)
-        }, 2000)
-    }, 3000);
+const makeMyCardWhite = () => {
+    businessCardElement.style.background = "white";
+};
+
+businessCardElement.addEventListener(
+    "click", 
+    function (event) {
+        console.log(event.x, event.y);
+        if (businessCardElement.style.background === "purple") {
+            makeMyCardWhite();
+        } else {
+            makeMyCardPurple();
+        }
+    }
+);
+
+phoneNumber.addEventListener("click", function (event) {
+    event.stopPropagation()
+    console.log("Phone was clicked!");
 })
 
-// setTimeout(() => {
-   
-// }, 3000);
+const buttonsSection = document.querySelector("#buttons-section");
 
-setInterval(() => {
-    const colors = ["red", "purple", "green", "blue"];
-    const chosenColor = colors[Math.floor(Math.random() * colors.length)]
-    myNameElement.style.color = chosenColor
-  }, 200);
+buttonsSection.addEventListener("click", function (event) {
+
+    const exactButtonThatWasClicked = event.target;
+
+    const action = exactButtonThatWasClicked.id;
+
+    if (action === "purple") {
+        makeMyCardPurple();
+    } else if (action === "white") {
+        makeMyCardWhite();
+    } else if (action === "spin") {
+        // todo
+    } else {
+        throw new Error("UNKNOWN ACTION")
+    }
+
+});
+
+setTimeout(() => {
+    const newButton = document.createElement("button");
+    newButton.innerText = "MAKE MY CARD WHITE";
+    newButton.id = "white";
+    buttonsSection.appendChild(newButton);
+}, 5000);
+
+// setTimeout(() => {
+//     makeMyCardPurple();
+// }, 5000);
+
